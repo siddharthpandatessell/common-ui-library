e43016d9fe0d41a10b3142543dcc3b36bdfbc894
Siddharth Panda <siddharthpanda@Siddharths-MacBook-Pro.local>
2023-06-20 17:51:16 +0530

    Restructured the MUI components to ensure reusability

--- src/stories/components/TessellModal/TessellModal.stories.tsx
+++ src/stories/components/TessellModal/TessellModal.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import TessellModal from './TessellModal';
+import renderFunctions from "./renderFunc";
 
 // Default metadata of the story https://storybook.js.org/docs/react/api/csf#default-export
 const meta: Meta<typeof TessellModal> = {
@@ -22,34 +23,22 @@ type Story = StoryObj<typeof TessellModal>;
 
 export const Primary: Story = {
   args: {
-    label: 'Primary',
-    variant: "contained",
-    size: "large",
-    color: "primary",
+    customStyles: {},
+    renderFunctions,
+    minWidth: "lg",
+    open: true,
+    onClose: () => () => null,
   },
 };
 
 export const Secondary: Story = {
-    parameters: {
-    docs: {
-      source: { language: 'tsx' },
-    },
     args: {
     ...Primary.args,
-    label: 'Secondary',
-    variant: "contained",
-    size: "medium",
-    color: "error",
-  },
   },
 };
 
 export const Modal: Story = {
   args: {
     ...Primary.args,
-    label: 'Modal',
-    variant: "outlined",
-    size: "medium",
-    color: "success",
   },
 };
--- src/stories/components/TessellModal/TessellModal.tsx
+++ src/stories/components/TessellModal/TessellModal.tsx
@@ -10,67 +10,8 @@ import {
   Box,
   Typography
 } from "@mui/material";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
 
-// import Close from '@mui/icons-material/Close';
-import React from "react";
-import TessellButton from "../TessellButton/TessellButton";
 
-const DialogTitleDefault = () => <h6>Test Modal</h6>;
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
-  >
-    •
-  </Box>
-);
-
-const DialogContentDefault = () => (<React.Fragment>
-  <CardContent>
-    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-      Word of the Day
-    </Typography>
-    <Typography variant="h5" component="div">
-      be{bull}nev{bull}o{bull}lent
-    </Typography>
-    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-      adjective
-    </Typography>
-    <Typography variant="body2">
-      well meaning and kindly.
-      <br />
-      {'"a benevolent smile"'}
-    </Typography>
-  </CardContent>
-  <CardActions>
-    <Button size="small">Learn More</Button>
-  </CardActions>
-</React.Fragment>);
-
-const DialogActionsDefault = () => (
-  <>
-    <TessellButton
-      label="Open"
-      color="primary"
-      size="small"
-      variant="contained"
-    ></TessellButton>
-    <TessellButton
-      label="Close"
-      color="primary"
-      size="small"
-      variant="contained"
-    ></TessellButton>
-  </>
-);
-
-const renderFunctions = {
-  title: DialogTitleDefault(),
-  content: DialogContentDefault(),
-  actions: DialogActionsDefault(),
-};
 
 const TessellModal = (props) => {
   const {
@@ -143,8 +84,8 @@ export default TessellModal;
 
 TessellModal.defaultProps = {
   customStyles: {},
-  renderFunctions: renderFunctions,
+  renderFunctions: {},
   minWidth: "lg",
   open: true,
-  onClose: () => {},
+  onClose: () => () => null,
 };
