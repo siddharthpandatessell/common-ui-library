fd8ab302cc81ad0c9edc295ebc004af4542742c0
Siddharth Panda <siddharthpanda@Siddharths-MacBook-Pro.local>
2023-06-20 17:41:19 +0530

    Added proper structure to modal using mui components

--- src/stories/components/TessellModal/TessellModal.tsx
+++ src/stories/components/TessellModal/TessellModal.tsx
@@ -1,34 +1,102 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
-import { Dialog, DialogActions, DialogContent, DialogTitle, Stack, Button } from '@mui/material';
-// import Close from '@mui/icons-material/Close';
-import React from 'react';
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Stack,
+  Button,
+  Box,
+  Typography
+} from "@mui/material";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
 
-const DialogTitleDefault = () => <h4>Test Modal</h4>;
-const DialogContentDefault = () => (
-       <p>Hello</p>
-    );
+// import Close from '@mui/icons-material/Close';
+import React from "react";
+import TessellButton from "../TessellButton/TessellButton";
+
+const DialogTitleDefault = () => <h6>Test Modal</h6>;
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+  >
+    •
+  </Box>
+);
+
+const DialogContentDefault = () => (<React.Fragment>
+  <CardContent>
+    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+      Word of the Day
+    </Typography>
+    <Typography variant="h5" component="div">
+      be{bull}nev{bull}o{bull}lent
+    </Typography>
+    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+      adjective
+    </Typography>
+    <Typography variant="body2">
+      well meaning and kindly.
+      <br />
+      {'"a benevolent smile"'}
+    </Typography>
+  </CardContent>
+  <CardActions>
+    <Button size="small">Learn More</Button>
+  </CardActions>
+</React.Fragment>);
+
 const DialogActionsDefault = () => (
-        <Button color="primary" size="small" variant="contained">
-            Close
-        </Button>
-    );
+  <>
+    <TessellButton
+      label="Open"
+      color="primary"
+      size="small"
+      variant="contained"
+    ></TessellButton>
+    <TessellButton
+      label="Close"
+      color="primary"
+      size="small"
+      variant="contained"
+    ></TessellButton>
+  </>
+);
 
 const renderFunctions = {
-        title: DialogTitleDefault(),
-        content: DialogContentDefault(),
-        actions: DialogActionsDefault(),
-    };
+  title: DialogTitleDefault(),
+  content: DialogContentDefault(),
+  actions: DialogActionsDefault(),
+};
 
 const TessellModal = (props) => {
-  const { customStyles = {}, renderFunctions, maxWidth, open, onClose, ...restProps } = props;
+  const {
+    customStyles = {},
+    renderFunctions,
+    maxWidth,
+    open,
+    onClose,
+    ...restProps
+  } = props;
 
   return (
     <>
       {open && (
-        <Dialog open={open} onClose={onClose} maxWidth={maxWidth} {...restProps}>
+        <Dialog
+          open={open}
+          onClose={onClose}
+          maxWidth={maxWidth}
+          {...restProps}
+        >
           <DialogTitle sx={customStyles?.title}>
-            <Stack direction="row" justifyContent="space-between" alignItems="center">
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+            >
               <Stack>{renderFunctions?.title}</Stack>
               {/* <Stack
             sx={{
@@ -59,7 +127,9 @@ const TessellModal = (props) => {
           </Fab> */}
             </Stack>
           </DialogTitle>
-          <DialogContent sx={customStyles?.content}>{renderFunctions?.content}</DialogContent>
+          <DialogContent sx={customStyles?.content}>
+            {renderFunctions?.content}
+          </DialogContent>
           <DialogActions spacing={1} sx={customStyles?.actions}>
             {renderFunctions?.actions}
           </DialogActions>
@@ -72,12 +142,9 @@ const TessellModal = (props) => {
 export default TessellModal;
 
 TessellModal.defaultProps = {
-  customStyles: {}, 
-  renderFunctions: renderFunctions, 
-  maxWidth: 'md', 
-  open: true, 
-  onClose: () => {}
+  customStyles: {},
+  renderFunctions: renderFunctions,
+  minWidth: "lg",
+  open: true,
+  onClose: () => {},
 };
-
-
-
